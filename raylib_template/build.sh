8a502f2c0c7316797ff9a98d7671b177bf048e9c
Dave Fishel <dave.fishel@gmail.com>
2023-10-27 22:53:29 -0700

    Update build scripts for linux

--- raylib_template/build.sh
+++ raylib_template/build.sh
@@ -1,5 +1,5 @@
 if [ "$(uname)" == "Darwin" ]; then
-  cmake -B build -DPLATFORM=Desktop -DCMAKE_BUILD_TYPE=Release -DUSE_EXTERNAL_GLFW=ON
+  cmake -B build -DPLATFORM=Desktop -DCMAKE_BUILD_TYPE=Release -DUSE_EXTERNAL_GLFW=OFF
   cd build
   make
   cd ..
@@ -9,8 +9,10 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
   # You might need to change this if you have a different version of VS installed
   MSBUILD_PATH="/c/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/"
   "${MSBUILD_PATH}"/MSBuild.exe ALL_BUILD.vcxproj -p:Configuration=Release
-  msbuild.exe ALL_BUILD.vcxproj -p:Configuration=Release
   cd ..
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  echo "Linux build not yet supported/tested"
+  cmake -B build -DPLATFORM=Desktop -DCMAKE_BUILD_TYPE=Release -DUSE_EXTERNAL_GLFW=OFF
+  cd build
+  make
+  cd ..
 fi
